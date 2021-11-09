4b75954d437e25d6cf0b41ebc5e532021f85669f
Cezary Bubieniec <pokion@outlook.com>
2021-11-09 18:29:30 +0100

    fix download
    fix issue with download

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const mime = require('mime');
 const fs = require('fs');
 const ytDownloader = require('./ytDownloader.js');
 let bodyParser = require('body-parser')
@@ -8,12 +7,44 @@ const app = express();
 const port = 8080;
 let jsonParser = bodyParser.json()
 let YT = new ytDownloader;
+let downloading = [];
 
 app.use(express.static('static'))
 app.use(jsonParser)
 
 app.get('/', (req, res)=>{
 	res.sendFile(path.join(__dirname, '/index.html'));
+})
+
+app.post('/sendvid', (req, res)=>{
+	let vidId = req.body.id
+	let index = req.body.index
+
+	if(downloading[index].percent.stage == "done"){
+		res.download(downloading[index].percent.path, vidId,(err)=>{
+			console.log(err)
+		})
+	}else{
+		res.status(404).send({
+			err: "Wrong request"
+		})
+	}
+	
+})
+
+app.post('/progress', (req, res)=>{
+	let vidId = req.body.id
+	let index = req.body.index
+
+	if(downloading[index].id == vidId){
+		res.send({
+			status: downloading[index]
+		})
+	}else{
+		res.send({
+			err: "Bad id video"
+		})
+	}
 })
 
 app.post('/download', (req, res)=>{
@@ -25,40 +56,40 @@ app.post('/download', (req, res)=>{
 			if(val.player_response.videoDetails.lengthSeconds <= 600){
 					let o = (req.body.trim) ? req.body.sliderVal[1] + "" : 100
 
+					let id = val.videoDetails.videoId
+					downloading.push({id, percent:0})
+					let place = 0;
+					downloading.forEach((el,index)=>{
+						if(el.id == id){
+							place = index
+							console.log(el,index)
+						}
+					})
+					res.send({
+						id,
+						index: place
+					})
+					console.log(downloading)
+					YT.download({
+						url: req.body.url,
+						directory: "./download/",
+						download: Math.floor(o) + "",
+						isPrecentage: !req.body.trim,
+						progress: (val)=>{console.log(val)
+							downloading[place].percent = val
+						},
+						onEnd: (val) => {
+							console.log('koniec',val);
+							let start = Math.floor(req.body.sliderVal[0]);
+							let duration = Math.abs(start - Math.floor(req.body.sliderVal[1]))
+							console.log(start, duration, "onEnd")
 
-				  YT.download({
-					url: req.body.url,
-					directory: "./download/",
-					download: Math.floor(o) + "",
-					isPrecentage: !req.body.trim,
-					progress: (val)=>{console.log(val)},
-					onEnd: (val) => {
-						console.log('koniec',val);
-						let start = Math.floor(req.body.sliderVal[0]);
-						let duration = Math.abs(start - Math.floor(req.body.sliderVal[1]))
-						console.log(start, duration, "onEnd")
+							YT.trim(val.path + val.fileName + "." + val.extension, start+"", duration+"", `./${val.fileName}.${val.extension}`, ()=>{
+								downloading[place].percent = {stage: "done", path: `./${val.fileName}.${val.extension}`};
 
-						YT.trim(val.path + val.fileName + "." + val.extension, start+"", duration+"", `./${val.fileName}.${val.extension}`, ()=>{
-
-							let file = `./${val.fileName}.${val.extension}`;
-							let filename = path.basename(file);
-							let mimetype = mime.getType(file);
-
-							res.download(__dirname + "/" + filename, filename, (err)=>{
-								fs.unlink(file, (err)=>{
-									if(err){
-										throw err;
-									}
-								})
-								fs.unlink(val.path + val.fileName + "." + val.extension, (err)=>{
-									if(err){
-										throw err;
-									}
-								})
 							})
-						})
-					}
-				  })
+						}
+					})
 			}else{
 				res.status(418).send({
 					err: "video is too long max 10 minutes"
--- static/ajax.js
+++ static/ajax.js
@@ -1,6 +1,38 @@
 //console.log(slider.noUiSlider.get())
+let progressInterval;
+let resp;
 
-function sendRequestToDownload(){
+function progress(){
+	let xmlhttp = new XMLHttpRequest();
+
+	xmlhttp.onreadystatechange = function(){
+		if(xmlhttp.readyState == XMLHttpRequest.DONE){
+			if(xmlhttp.status == 200){
+				let response = JSON.parse(xmlhttp.response);
+				console.log(response)
+				console.log(typeof(response.status.percent))
+				if(typeof(response.status.percent) == "object"){
+					console.log('no jest')
+					console.log(typeof(response.status.percent))
+					clearInterval(progressInterval);
+					download()
+				}
+			}else {
+				clearInterval(progressInterval)
+				let response = JSON.parse(xmlhttp.response);
+				loader.classList.toggle('noVisibility');
+				alert(response.err)
+			}
+		}
+	};
+	let data = JSON.stringify(resp)
+
+	xmlhttp.open("POST", "/progress", true);
+	xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+	xmlhttp.send(data)
+}
+
+function download(){
 	let xmlhttp = new XMLHttpRequest();
 	let url = document.querySelectorAll('input[name="url"]')[0].value;
 	let sliderVal = slider.noUiSlider.get();
@@ -26,6 +58,38 @@ function sendRequestToDownload(){
 	            center0.classList.toggle('noVisibility');
 				center1.classList.toggle('noVisibility');
 				url.value = "";
+
+			}else {
+				let response = JSON.parse(xmlhttp.response);
+				loader.classList.toggle('noVisibility');
+				alert(response.err)
+			}
+		}
+	};
+	let data = JSON.stringify(resp)
+
+	xmlhttp.open("POST", "/sendvid", true);
+	xmlhttp.responseType = "blob";
+	xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+	xmlhttp.send(data)
+}
+
+function sendRequestToDownload(){
+	let xmlhttp = new XMLHttpRequest();
+	let url = document.querySelectorAll('input[name="url"]')[0].value;
+	let sliderVal = slider.noUiSlider.get();
+	let trim = document.querySelectorAll('input[name="trim"]')[0].checked;
+	let title = document.querySelectorAll('#title')[0];
+	let center1 = document.querySelectorAll('.center')[1];
+	let center0 = document.querySelectorAll('.center')[0];
+	loader.classList.toggle('noVisibility');
+
+	xmlhttp.onreadystatechange = function(){
+		if(xmlhttp.readyState == XMLHttpRequest.DONE){
+			if(xmlhttp.status == 200){
+				resp = JSON.parse(xmlhttp.response);
+				progressInterval = setInterval(progress, 1000)
+
 			}else {
 				let response = JSON.parse(xmlhttp.response);
 				loader.classList.toggle('noVisibility');
@@ -40,7 +104,6 @@ function sendRequestToDownload(){
 	})
 
 	xmlhttp.open("POST", "/download", true);
-	xmlhttp.responseType = "blob";
 	xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 	xmlhttp.send(data)
 }
